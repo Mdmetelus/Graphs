b5d27d78e1bfd94b5fc1f90137f444fca1b20c39
Mr. Metelus <Mdmetelus@gmail.com>
2019-04-09 15:46:19 -0400

    update

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -141,7 +141,54 @@ class Graph:
                 else:
                     return print("they don't connect")
 
-                    
+       # Debth first traversal with recusion
+    def dft_r(self, start_vert, visited = None):
+        if visited is None:
+            visited = set()
+        visited.add(start_vert)
+        print(start_vert)
+        for child_vert in self.vertices(start_vert):
+            if child_vert not in visited:
+                self.dft_r(child_vert, visted) # recusive call
+
+# breath first search with path returned at the end (no recursion possible)
+    def bfs_path(self, starting_vertex_id, target_value):
+      q = Queue()
+      q.enqueue([starting_vertex_id])
+      visited = set()
+      while q.size() > 0:
+          path = q.dequeue()
+          v = path[-1]
+          if v not in visited:
+              if v == target_value:
+                  return path
+              visited.add(v)
+              for next_vert in self. vertices[v]
+                  new_path = list(path)
+                  # or new_path = path.copy()
+                  new_path.append(next_vert)
+                  q.enqueue(new_path)
+      return None
+
+# recursive dfs traversal while returning the path
+    def dfs_r_path(self, start_vert, target_value, visited = None, path = none):
+        if visited is None:
+           visited = set()
+        if path = None:
+            path = []
+        visited.add(start_vert) # being used as argument and not being assessed so non brackets needed around start_vert here.
+        path = path + [start_vert]
+        if start_vert == target_value:
+            return path
+        for child_vert in self.vertices[start_vert]:
+            if child_vert not in visited:
+                new_path = self.dfs_r_path(child_vert, target_value, visited, path)
+                if new_path:
+                    return new_path
+        return None
+
+
+
 
 
 graph = Graph()  # Instantiate your graph
